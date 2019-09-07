cbe9f0f14c46da4bc6eeb91fd85dac4b6a855543
Roy Sun <rs58@rice.edu>
2019-09-07 14:54:18 -0500

    add p0 files

---
+++ P0/src/BSTTree.java
@@ -0,0 +1,11 @@
+public class BSTTree<T extends Comparable<T>> implements BinarySearchTreeADT<T> {
+
+    private BSTNode<T> root;
+    private int size;
+
+    public BSTTree() {
+        root = null;
+    }
+
+    // you will write the rest of this class
+}
