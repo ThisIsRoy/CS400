ece58f04a2a75ae1a8779cb93f8d24f00e004287
Roy Sun <rs58@rice.edu>
2019-09-22 23:55:52 -0500

    p2
    p2

---
+++ P2/src/AVLTree.java
@@ -0,0 +1,310 @@
+// remember that you are not allowed to add any public methods or fields
+// but you can add any private methods or fields
+
+public class AVLTree<K extends Comparable<K>, V> implements TreeADT<K, V> {
+	public TreeNode<K, V> root = null;
+
+	/** 
+	 * Inner class representing a node of the AVL tree. 
+	 * @param <K>
+	 * @param <V>
+	 */
+	private class TreeNode<K, V> {
+		private K key;
+		private V value;
+		private int height;
+		private TreeNode<K, V> left, right;
+
+		/**
+		 * constructor for node
+		 * @param key key of node
+		 * @param value value of node
+		 */
+		private TreeNode(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		/**
+		 * get key of node
+		 * @return key of node
+		 */
+		public K getKey() {
+			return key;
+		}
+
+		/**
+		 * get value of node
+		 * @return value of node
+		 */
+		private V getValue() {
+			return value;
+		}
+
+		/**
+		 * set height of node
+		 * @param height integer height to set
+		 */
+		private void setHeight(int height) {
+			this.height = height;
+		}
+
+		/**
+		 * get left child of node
+		 * @return left child of node
+		 */
+		private TreeNode<K, V> getLeft() {
+			return left;
+		}
+
+		/**
+		 * set left child of node
+		 * @param left new left child of node
+		 */
+		private void setLeft(TreeNode<K, V> left) {
+			this.left = left;
+		}
+
+		/**
+		 * get right child of node
+		 * @return right child of node
+		 */
+		private TreeNode<K, V> getRight() {
+			return right;
+		}
+
+		/**
+		 * set right child of node
+		 * @param right new right child of node
+		 */
+		private void setRight(TreeNode<K, V> right) {
+			this.right = right;
+		}
+
+		/**
+		 * get height of node
+		 * @return height of node
+		 */
+		private int getHeight() {
+			if (this.left == null && this.right == null) {
+				return 1;
+			} else if (this.left == null) {
+				return 1 + this.right.getHeight();
+			} else if (this.right == null) {
+				return 1 + this.left.getHeight();
+			} else {
+				return 1 + Math.max(this.left.getHeight(), this.right.getHeight());
+			}
+		}
+
+		/**
+		 * get balance factor of node, left child height minus right child height
+		 * @return balance factor of node
+		 */
+		private int getBalanceFactor() {
+			if (this.left == null && this.right == null) {
+				return 0;
+			} else if (this.left == null) {
+				return -1 * this.right.getHeight();
+			} else if (this.right == null) {
+				return this.left.getHeight();
+			} else {
+				return this.left.getHeight() - this.right.getHeight();
+			}
+		}
+	}
+
+   // TODO:  IMPLEMENT ALL INTERFACE METHODS
+	/**
+	 * Checks for an empty tree.
+	 * @return true if tree contains 0 items
+	 */
+	public boolean isEmpty() {
+		return root == null;
+	};
+
+	/**
+	 * Adds a key, value pair to the tree.
+	 * @param key
+	 * @param value
+	 * @throws DuplicateKeyException if the key has already been inserted into the tree
+	 * @throws IllegalKeyException if the key is null
+	 */
+
+	public void insert(K key, V value) throws DuplicateKeyException, IllegalKeyException {
+		if (key == null) {
+			throw new IllegalKeyException();
+		}
+
+		if (root == null) {
+			root = new TreeNode<K, V>(key, value);
+		}
+
+		insert(root, key, value);
+	};
+
+	/**
+	 * helper function for inserting node
+	 * @param currNode current node
+	 * @param key key to insert
+	 * @param value value to insert
+	 * @throws DuplicateKeyException null key error
+	 */
+	private void insert(TreeNode<K, V> currNode, K key, V value) throws DuplicateKeyException {
+		K currKey = currNode.getKey();
+		if (currKey.compareTo(key) == 0) {
+			throw new DuplicateKeyException();
+		}
+
+		// key is less than current key, go left
+		if (key.compareTo(currKey) < 0) {
+			if (currNode.getLeft() == null) {
+				currNode.setLeft(new TreeNode<K, V>(key, value));
+			} else {
+				insert(currNode.getLeft(), key, value);
+			}
+
+			// key is greater than current key, go right
+		} else if (key.compareTo(currKey) > 0) {
+			if (currNode.getRight() == null) {
+				currNode.setRight(new TreeNode<K, V>(key, value));
+			} else {
+				insert(currNode.getRight(), key, value);
+			}
+		}
+
+		// check balance factor after insertion
+		int balanceFactor = currNode.getBalanceFactor();
+		if (Math.abs(balanceFactor) > 1) {
+			balance(currNode, balanceFactor);
+		}
+	}
+
+	/**
+	 * helper function for balancing node
+	 * assumes balance factor of current node is 2 or greater
+	 * @param currNode current node
+	 * @param balanceFactor balance factor of current node
+	 */
+	private void balance(TreeNode<K, V> currNode, int balanceFactor) {
+		if (balanceFactor > 0) {
+			int leftBalanceFactor = currNode.getLeft().getBalanceFactor();
+
+			// left left case
+			if (leftBalanceFactor > 0) {
+				rotateRight(currNode);
+
+				// left right case
+			} else {
+
+			}
+		} else {
+			int rightBalanceFactor = currNode.getRight().getBalanceFactor();
+
+			// right left case
+			if (rightBalanceFactor > 0) {
+
+				// right right case
+			} else {
+
+			}
+		}
+	}
+
+	/**
+	 * Removes a key from the AVL tree. Returns nothing if the key is not found.
+	 * If a node has 2 children, replace that node with the inorder predecessor
+	 * @param key
+	 * @throws IllegalKeyException if attempt to delete null
+	 */
+	public void delete(K key) throws IllegalKeyException {
+		if (key == null) {
+			throw new IllegalKeyException();
+		}
+
+		// TODO finish delete
+	};
+   
+	/**
+	 * AVLTree rotate left.
+	 * @param root an imbalance node
+	 * @return TreeNode<K, V> the node for which balance has been modified 
+	 */
+	private TreeNode<K, V> rotateLeft(TreeNode<K, V> root) {
+		TreeNode<K, V> rightChild = root.getRight();
+		TreeNode<K, V> rightLeftChild = null;
+
+		// store root's right child's left child if it exists
+		if (rightChild.getLeft() != null) {
+			rightLeftChild = rightChild.getLeft();
+		}
+
+		root.setRight(rightLeftChild);
+		rightChild.setLeft(root);
+
+		return rightChild;
+	}
+
+	/**
+	 * AVLTree rotate right.
+	 * @param root an imbalance node
+	 * @return the node for which balance has been modified 
+	 */
+	private TreeNode<K, V> rotateRight(TreeNode<K, V> root) {
+		// TODO: implement
+		return root;
+	}
+
+	/**
+	 * Get a value in the tree given the key.
+	 * @param key
+	 * @return value  the object associated with this key
+	 * @throws IllegalArgumentException if the key is null
+	 */
+	public V get(K key) throws IllegalKeyException {
+		if (key == null) {
+			throw new IllegalKeyException();
+		}
+
+		return get(root, key);
+	};
+
+	private V get(TreeNode<K, V> currNode, K key) {
+		K currKey = currNode.getKey();
+
+		if (currKey.compareTo(key) == 0) {
+			return currNode.getValue();
+
+			// key is smaller, look left
+		}  else if (key.compareTo(currKey) < 0) {
+			return get(currNode.getLeft(), key);
+
+			// key is larger, look right
+		} else {
+			return get(currNode.getRight(), key);
+		}
+	}
+
+	/**
+	 * Print a tree sideways to show structure. This code is completed for you.
+	 */
+	public void printSideways() {
+		System.out.println("------------------------------------------");
+		recursivePrintSideways(root, "");
+		System.out.println("------------------------------------------");
+	}
+
+	/**
+	 * Print nodes in a tree. This code is completed for you. 
+	 * You are allowed to modify this code to include balance factors or heights
+	 * @param current
+	 * @param indent
+	 */
+	private void recursivePrintSideways(TreeNode<K, V> current, String indent) {
+		if (current != null) {
+			recursivePrintSideways(current.right, indent + "    ");
+			System.out.println(indent + current.key);
+			recursivePrintSideways(current.left, indent + "    ");
+		}
+	}
+}
